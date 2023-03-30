43bd27a98eb957818875d39432e575316c0179b0
Lin Oo <linoo.koko.dev@gmail.com>
2023-03-30 12:17:38 +0630

    Test Query

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: [
+    'simple-import-sort',
+    'prettier',
+    '@typescript-eslint/eslint-plugin',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/accessible-emoji': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
--- src/pages/estate/[estateId].tsx
+++ src/pages/estate/[estateId].tsx
@@ -1,51 +1,76 @@
-import api from '@/configs/api';
 import { Estate, ResponseData } from '@/configs/types';
-import { GetServerSideProps } from 'next';
+import api from '@/configs/api';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useQuery, QueryClient, UseQueryResult, dehydrate, DehydratedState } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 
-export const getServerSideProps: GetServerSideProps<ResponseData<Estate>> = async (context) => {
-
-    const { estateId } = context.query;
-    const result: ResponseData<Estate> = await api.get<ResponseData<Estate>>(`estates/${estateId}`)
+export const fetchDetail = async (estateId: string | string[] | undefined) => {
+    return api.get<ResponseData<Estate>>(`estates/${estateId}`)
         .then(response => response.data);
-
-    return {
-        props: result,
-    }
 }
 
-const EstateDetail = ({ message, response, status }: ResponseData<Estate>) => {
+interface ErrorProps {
+    message: string;
+}
 
-    if (status === 200)
+const Error = (props: ErrorProps) => (
+    <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+        <span>
+            <h1>Error</h1>
+            <h4>{props.message.toString()}</h4>
+        </span>
+    </div>
+)
+
+const EstateDetail = () => {
+
+    const router = useRouter();
+    const { estateId } = router.query;
+
+    console.log(router.isReady, estateId)
+
+    const { data, isLoading } = useQuery({
+        queryKey: ["EstateDetail", estateId],
+        queryFn: () => {
+            if (router.isReady) return fetchDetail(estateId);
+
+            return null;
+        },
+        staleTime: Infinity,
+    });
+
+    if (isLoading) return <h1>Loading</h1>;
+
+    if (data?.status == 200) {
+
         return (
             <>
                 <Head>
-                    <title>{response?.township?.name}</title>
-                    <meta name="title" content={response?.township?.name} />
-                    <meta name="description" content={response?.price} />
+                    <title>{data?.response?.township?.name}</title>
+                    <meta name="title" content={data?.response?.township?.name} />
+                    <meta name="description" content={data?.response?.price} />
 
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://nextjs-test-7mrq72fej-koko-linoo.vercel.app/" />
-                    <meta property="og:title" content={response?.township?.name} />
-                    <meta property="og:description" content={response?.price} />
+                    <meta property="og:title" content={data?.response?.township?.name} />
+                    <meta property="og:description" content={data?.response?.price} />
                     <meta property="og:image" content="https://b-plus-storage-staging.s3.ap-southeast-1.amazonaws.com/1680060586457-28844052-istockphoto-511061090-612x612.jpg" />
 
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content="https://nextjs-test-7mrq72fej-koko-linoo.vercel.app/about" />
-                    <meta property="twitter:title" content={response?.township?.name} />
-                    <meta property="twitter:description" content={`MMK ${response?.price ?? 0}`} />
-                    {response?.files?.length ?
+                    <meta property="twitter:title" content={data?.response?.township?.name} />
+                    <meta property="twitter:description" content={`MMK ${data?.response?.price ?? 0}`} />
+                    {data?.response?.files?.length ?
                         <meta
                             property="twitter:image"
-                            content={response?.files[0].url ?? ""}
+                            content={data?.response?.files[0].url ?? ""}
                         /> : null
                     }
                 </Head>
                 <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
                     <span>
                         <div style={{ display: "flex", justifyContent: "center" }}>
-                            {response?.files?.map(file => (
+                            {data?.response?.files?.map(file => (
                                 <img
                                     width={300}
                                     height={300}
@@ -56,24 +81,17 @@ const EstateDetail = ({ message, response, status }: ResponseData<Estate>) => {
                             ))}
                         </div>
                         <br />
-                        <h1>{response?.township?.name}</h1>
+                        <h1>{data?.response?.township?.name}</h1>
                         <br />
-                        <h2>{response?.address}</h2>
+                        <h2>{data?.response?.address}</h2>
                         <br />
-                        <h2>MMK {response?.price}</h2>
+                        <h2>MMK {data?.response?.price}</h2>
                     </span>
                 </div>
             </>
         )
-
-    return (
-        <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
-            <span>
-                <h1>Error</h1>
-                <h4>{message}</h4>
-            </span>
-        </div>
-    )
+    }
+    return <Error message={data?.message?.toString() ?? ""} />
 }
 
 export default EstateDetail;
